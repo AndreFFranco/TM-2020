b98d9ababefadadd7d97fddab7e6432df5a4609a
Andre Franco <andrefranco@ipvc.pt>
2020-03-02 16:30:33 +0000

    AndreFranco-19500

---
+++ Aula 2/exercicio2/AndreFranco-19500/script.js
@@ -0,0 +1,15 @@
+var arrayNumbers = [];
+
+function adicionarNumero() {
+    var numero = document.getElementById("numbersId").value;
+    arrayNumbers.push(parseInt(numero));
+    console.log(arrayNumbers);
+    document.getElementById("numerosAdicionados").innerText = arrayNumbers.toString();
+}
+
+function calcularNumero() {
+    if(arrayNumbers.length > 4) {
+        var maiorNumero = Math.max.apply(Math, arrayNumbers);
+        alert(maiorNumero);
+    }
+}
